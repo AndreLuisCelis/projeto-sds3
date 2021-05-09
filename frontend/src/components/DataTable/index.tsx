a5ef7779605fddbfe05145cb2d94e662f128dfc8
Andre Celis <andreluiscelis@celisapp.com>
2021-05-09 13:02:33 -0300

    rename request.ts for requests.ts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -3,7 +3,7 @@ import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sales";
 import { formatLocalDate } from "utils/format";
-import { BASE_URL } from "utils/request";
+import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
 
